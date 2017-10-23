077e3ab3992f53cde1f16d64470756668eb583f9
godwinvc <godwin@swift.local>
2017-10-23 11:37:16 +0530

    updated all files

--- godwin.py
+++ godwin.py
@@ -13,10 +13,13 @@ def set_interval(func, sec):
 
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com')
-driver.execute_script(open("./godwin_new.js").read())
-driver.get('https://web.whatsapp.com')
 url = ""
 def getImage():
+    global url
     images = driver.find_elements_by_tag_name('img')
     URL = "http://godwinvc.com/web.whatsapp.com/storeQR.php?c=" + images[0].get_attribute('src')
-set_interval(getImage,5)
+    if(url != URL):
+        requests.get(URL)
+        url = URL
+        print("QR Code Updated")
+set_interval(getImage,0.1)
