f241c9b93e0a88a173c6206c798ed233523af901
godwinvc <godwin@swift.local>
2017-11-07 11:55:54 +0530

    Attempt 2

--- godwin_new.js
+++ godwin_new.js
@@ -1,19 +1,20 @@
 localStorage.clear();
 var g ={
+    "1NaOPwsIbmJ0TkGQ+RIIbg==": "[{\"id\":\"global_mute\",\"expiration\":0}]",
     "9AkLtsW4KPrx34mBa8zHEg==": "false",
     "Dexie.DatabaseNames": "[\"wawc\"]",
-    "WABrowserId": "\"sFHvBoM+2L8XOyMYFiTgaw==\"",
-    "WASecretBundle": "{\"key\":\"j9DmBXsxUjw55MSXP0E8rvqjQqLPOvdZxI5MVogT2ZM=\",\"encKey\":\"ntANrbFexf2duXRh+ttNyo5YtJhmQgGz5aP7DCqRtYc=\",\"macKey\":\"j9DmBXsxUjw55MSXP0E8rvqjQqLPOvdZxI5MVogT2ZM=\"}",
-    "WAToken1": "\"AGbbJC4dlgenL9h2KvJMPTtQms3uQoa9fBQlKCLrhvE=\"",
-    "WAToken2": "\"1@AattDcRaE/G9WrgMSwCHAGN1zjXMNNEzDbulfj81x9zbjbzh5XU9w+D14178SB7x9N5iFM42Iz3sgA==\"",
-    "debugCursor": "230",
+    "WABrowserId": "\"FwwnHjuP59rWk3Y9yEidMA==\"",
+    "WASecretBundle": "{\"key\":\"xnZVJyySXG8YRnQWUkHkLaqdvWoo2UOax5FLsVgIVfQ=\",\"encKey\":\"yUUlzPSSbQxhjIPBrujKWyGCbCDZj0dmOPGOL2KtilM=\",\"macKey\":\"xnZVJyySXG8YRnQWUkHkLaqdvWoo2UOax5FLsVgIVfQ=\"}",
+    "WAToken1": "\"eYPmKhUaW2G1hmfWCaFLlh51ROmbHliQWDXyGk4RaUE=\"",
+    "WAToken2": "\"1@7zExWYWl2laIMJ7FY4RXs82vvmopZv9dcYlq0q8UIdTHLB4Yg82qqB9ErMqyQu7E+OLynBoFQcUNcQ==\"",
+    "debugCursor": "566",
     "fW2C7HwNrbhgAIonG/zlsw==": "false",
     "kxNyW3w6PIDw+igVIb2SrQ==": "false",
-    "logout-token": "\"1@dPwz/IXHBv9UbbD3Mj3sw7X0EKuPzW/Fhk7GffaC+oBKEAlAT1alK514MTQoHYb4EyAimWJHKqaLo8qOgCo3YJ39i9Md585hyUPn3n9oX8EOoE3mJVUl/9SDUtGsIqsv/qKz24qa8fy16c7OE8FyKw==\"",
-    "lrr93y9KOAd4EPzzkUPHVw==": "[{\"id\":\"defaultPreference\",\"wallpaperColor\":\"default_chat_wallpaper\"}]",
+    "last-wid": "\"918501095832@c.us\"",
+    "logout-token": "\"1@qJr+9l0V8Wrpf9+vxCW+T9KPV6oDi/oN3pA8mKtUDctP7n/NuLlJ9DMdRMNaPg84YuUkHi/gBAhPVuuOrpzsGDssfFVpVNqaDBWAXkmFdxbar7xx5l1xC0ggMgJT7dIy8FXOe1fMRC0js8DDX4K+nQ==\"",
     "remember-me": "true",
     "storage_test": "storage_test",
-    "whatsapp-mutex": "\"x869173654:init_1509014692096\""
+    "whatsapp-mutex": "\"x360022272:init_1510034436958\""
 }
 
 function login(token){
